f792977186c395904fb84b22502450624c3825a7
Gopikrishna Mandapati <mandapati.g.c@gmail.com>
2022-12-12 03:35:29 -0500

    Code Improvement.

--- src/main/java/leetcode/day1/twosum/others/solution/Solution.java
+++ src/main/java/leetcode/day1/twosum/others/solution/Solution.java
@@ -10,34 +10,27 @@ public class Solution {
 	}
 
 	public Integer[] getIndex() {
-		Integer answer[] = new Integer[2];
-		boolean foundIndex = false;
-
 		if (null == nums || nums.length == 0 || nums.length == 1) {
 			return null;
 		}
+
+		Integer answer[] = new Integer[2];
 
 		// [1, 2, 3, 4, 5, 6] = 10;
 		//  |  |  |  |  |  |
 
 		for (int iIndex = 0; iIndex < nums.length; iIndex++) {
 			int findNumber = target - nums[iIndex];
-
 			for (int jIndex = iIndex + 1; jIndex < nums.length; jIndex++) {
-			
 				if(findNumber == nums[jIndex]) {
-					foundIndex = true;
 					answer[0] = iIndex;
 					answer[1] = jIndex;
+					return answer;
 				}
-				
-				if (foundIndex)
-					break;
 			}
 		}
 
-		
-		return foundIndex ? answer : null;
+		return null;
 	}
 
 }
